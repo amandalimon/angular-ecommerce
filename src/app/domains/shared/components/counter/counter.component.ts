0756a51700483c34730f9e0d0de59d0aa2c157f0
amandalimon <amandalimon@outlook.com>
2024-06-24 13:25:02 -0600

    angular components lifecycle

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({ required: true }) duration: number = 0;
+  @Input({ required: true }) message: string = '';
+
+  constructor() {
+    // NO ASYNC
+    // before render
+    // una vez
+    console.log('constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // before and during render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    // after render
+    // una vez
+    // async, then, subs
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration =>', this.duration)
+    console.log('message =>', this.message)
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // hijos ya fueron renderizados
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+  }
+}
