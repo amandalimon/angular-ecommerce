6f652d28c94257d3aff26aa424dca0404abff4ab
amandalimon <amandalimon@outlook.com>
2024-06-29 14:14:34 -0600

    nested views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from '@shared/components/header/header.component';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
-  template: '<app-header/><router-outlet/>`',
+  template: '<router-outlet/>`',
 })
 export class AppComponent {
   title = 'Angular Goods';
