5646982c06dc962046c17f597c03e484deff674e
amandalimon <amandalimon@outlook.com>
2024-06-29 12:51:29 -0600

    not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
+import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 
 export const routes: Routes = [{
   path: '',
@@ -9,4 +10,8 @@ export const routes: Routes = [{
 {
   path: 'about',
   component: AboutComponent
+},
+{
+  path: '**',
+  component: NotFoundComponent
 }];
